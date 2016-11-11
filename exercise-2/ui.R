11e3ebf83ca1eb22444d598fdcdf6f6b4783b2f2
BrydenR <bryden.m.robertson@gmail.com>
2016-11-10 22:53:50 -0800

    Added code.

--- exercise-2/ui.R
+++ exercise-2/ui.R
@@ -1,15 +1,15 @@
 # UI for scatterplot
-
+require(shiny)
 # Create a shinyUI with a fluidPage layout
 shinyUI(fluidPage(
   
-  # Add a numbericInput to choose the number of observations
-  
+  # Add a numericInput to choose the number of observations
+  numericInput("num", label="Number of observations", value = 100),
   
   # Add a selectInput that allows you to select a color from a list of choices
-  
+  selectInput('color', label = 'Color', choices = list("Red" = 'red', 'Blue' = 'blue', 'Green' = 'green')),
   
   # Plot the output with the name "scatter"
-  
+  plotOutput('scatter')
   
 ))
